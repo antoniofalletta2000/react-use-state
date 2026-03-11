4fd1261058fa187fa97befc03883841786c90f1a
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-11 13:05:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <AppHeader/>
+      <AppMain/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,11 @@
+export default function AppHeader(){
+
+    return(
+           <header className="p-2 bg-secondary-subtle">
+              <h1 className="text-dark">Learn Web development</h1>
+           </header>
+    )
+        
+            
+
+}
